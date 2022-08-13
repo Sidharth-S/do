87da7ff7f336165989acd5aa0e242bd8b1219115
Sidharth S <sidharthsud2701@gmail.com>
2022-08-13 14:37:44 +0530

    v1.22
    Change in web and new icon

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ executables = [Executable(
 
 setup(
     name="do",
-    version="1.21",
+    version="1.22",
     author = "Sidharth S",
     description="Customizable macro tasker",
     executables=executables,
